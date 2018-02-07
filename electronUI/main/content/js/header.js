d151f26d6b79da0676bf216cbd3c96b31586e979
Shaun Keys <mariobuddy@gmail.com>
2018-02-07 08:40:40 -0700

    Fixed page permanently disappearing when clicking empty space in sidebar

--- electronUI/main/content/js/header.js
+++ electronUI/main/content/js/header.js
@@ -1,9 +1,13 @@
 document.querySelector(".sidebar").addEventListener("click",function(e) {
-	//Hide current page and mark it inactive
-	var oldActive = document.querySelector("div.item.active");
-	oldActive.classList.remove("active");
-	document.querySelector(`#${oldActive.getAttribute("for")}`).classList.remove("active");
-	//Mark page associated with click as active
-	e.target.classList.add("active");
-	document.querySelector(`#${e.target.getAttribute("for")}`).classList.add("active");
+	
+	if(e.target.classList.contains("item")) {
+		//Hide current page and mark it inactive
+		var oldActive = document.querySelector("div.item.active");
+		oldActive.classList.remove("active");
+		document.querySelector(`#${oldActive.getAttribute("for")}`).classList.remove("active");
+		//Mark page associated with click as active
+		e.target.classList.add("active");
+		document.querySelector(`#${e.target.getAttribute("for")}`).classList.add("active");
+	}
+
 })
