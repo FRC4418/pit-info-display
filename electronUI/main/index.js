1562ec9d11caaf9e6bb9a6db641029323805eb04
Shaun Keys <mariobuddy@gmail.com>
2018-02-07 09:36:45 -0700

    Made window only appear after everything has loaded.

--- electronUI/main/index.js
+++ electronUI/main/index.js
@@ -1,11 +1,18 @@
 //Get the path module for use in naming
-var {BrowserWindow} = require("electron");
+var {BrowserWindow, ipcMain} = require("electron");
 var path = require("path");
 var windowName = path.basename(__dirname);
 
 module.exports = function() {
 	windows.set(windowName,new BrowserWindow({
-		fullscreen: true
+		fullscreen: false,
+		show: false
 	}));
 	windows.get(windowName).loadURL(`file://${__dirname}/content/index.html`);
+	ipcMain.on("status",function(e,arg) {
+		if(arg=="everything-is-loaded") {
+			windows.get(windowName).setFullScreen(true);
+			windows.get(windowName).show();
+		}
+	});
 }
