5d4a4eda9a8cca73680388fe9535d59b654382f5
Shaun Keys <mariobuddy@gmail.com>
2018-02-08 23:32:07 -0700

    Added team class and got config and tba instances

--- electronUI/main/content/js/setupEnv.js
+++ electronUI/main/content/js/setupEnv.js
@@ -1,2 +1,21 @@
 var electron = require("electron");
 var ipc = electron.ipcRenderer;
+var remote = electron.remote;
+var cfg = remote.getGlobal("cfg");
+var tba = remote.getGlobal("tba");
+
+class Team {
+	constructor(teamNumber,enabled) {
+		this.number = 0;
+		this.name = "LOADING...";
+		this.rank = 0;
+		this.enabled = true;
+		let self = this;
+		tba.getTeam(teamNumber).then((team) => {//Get team data
+			self.number = teamNumber;
+			self.name = team.nickname;
+			self.rank = 0;
+			self.enabled = true
+		});
+	}
+}
