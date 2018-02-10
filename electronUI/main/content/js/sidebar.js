0a9fa7879390ea6ce34b96d4f6c280c537b4fed7
Shaun Keys <mariobuddy@gmail.com>
2018-02-10 15:15:30 -0700

    Made it look better and handle page switching better

--- electronUI/main/content/js/sidebar.js
+++ electronUI/main/content/js/sidebar.js
@@ -1,13 +1,17 @@
 document.querySelector(".sidebar").addEventListener("click",function(e) {
-	
+
 	if(e.target.classList.contains("item")) {
 		//Hide current page and mark it inactive
 		var oldActive = document.querySelector("div.item.active");
 		oldActive.classList.remove("active");
-		document.querySelector(`#${oldActive.getAttribute("for")}`).classList.remove("active");
+		var oldActivePage = document.querySelector(`#${oldActive.getAttribute("for")}`);
+		oldActivePage.classList.remove("active");
+		oldActivePage.classList.add("hidden");
 		//Mark page associated with click as active
 		e.target.classList.add("active");
-		document.querySelector(`#${e.target.getAttribute("for")}`).classList.add("active");
+		var newActive = document.querySelector(`#${e.target.getAttribute("for")}`);
+		newActive.classList.remove("hidden");
+		newActive.classList.add("active");
 	}
 
 })
