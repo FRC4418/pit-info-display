2d0ab6671cd266a2d66a4409f517f2818aab1638
Shaun Keys <mariobuddy@gmail.com>
2018-02-10 15:18:24 -0700

    Isolated variables

--- electronUI/main/content/js/main.js
+++ electronUI/main/content/js/main.js
@@ -1,37 +1,40 @@
 // Render SASS files
 // (makes it easier for people who don't know how or care to compile them)
-var autoCompile = true;
-if(autoCompile) {
-	(function() {
-		var sass = require("node-sass");
-		sass.render({
-			file: `${process.cwd()}/electronUI/main/content/sass/main.scss`
-		},function(err,res) {
-			//console.log(err,String(res.css));
-			var css = document.createElement("style");
-			css.textContent = String(res.css);
-			document.head.appendChild(css);
-		});
-	})();
-}
+(function() {
+	var autoCompile = true;
+	if(autoCompile) {
+		(function() {
+			var sass = require("node-sass");
+			sass.render({
+				file: `${process.cwd()}/electronUI/main/content/sass/main.scss`
+			},function(err,res) {
+				//console.log(err,String(res.css));
+				var css = document.createElement("style");
+				css.textContent = String(res.css);
+				document.head.appendChild(css);
+			});
+		})();
+	}
+})();
 
-var scriptsToLoad = [ //This list is in order (scripts loaded synchronously (NOT RUN SYNCHRONOUSLY))
-	"https://cdn.jsdelivr.net/npm/vue",
-	"js/setupEnv.js",
-	"js/header.js",
-	"js/contentControl/nextMatch.js",
-	"js/showWindow.js"
-];
-
-scriptsToLoad.reverse();
-
-function loadScripts() {
-	var script = document.createElement("script");
-	script.src = scriptsToLoad.pop();
-	script.onload = function() {
-		if(scriptsToLoad.length > 0) loadScripts();
-	};
-	document.head.appendChild(script);
-}
-
-window.onload = loadScripts;
+// Load everything else
+(function() {
+	var scriptsToLoad = [ //This list is in order (scripts loaded synchronously (NOT RUN SYNCHRONOUSLY))
+		"https://cdn.jsdelivr.net/npm/vue",		//Vue library
+		"js/setupEnv.js",						//Set up envoronment (mostly electron stuff)
+		"js/sidebar.js",						//Set up the sidebar to switch pages
+		"js/contentControl/nextMatch.js",		//Set up the vue controller for #NextMatch
+		"js/contentControl/stats.js",			//Set up the vue controller for #TeamStats
+		"js/showWindow.js"						//Tell the main process we are done loading
+	];
+	scriptsToLoad.reverse();
+	function loadScripts() {
+		var script = document.createElement("script");
+		script.src = scriptsToLoad.pop();
+		script.onload = function() {
+			if(scriptsToLoad.length > 0) loadScripts();
+		};
+		document.head.appendChild(script);
+	}
+	window.onload = loadScripts;
+})();
