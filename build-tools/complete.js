e366a46c5b40713a619c4848740b1b66d56a8786
Shaun Keys <mariobuddy@gmail.com>
2018-03-22 20:48:36 -0600

    Make it work with windows

--- build-tools/complete.js
+++ build-tools/complete.js
@@ -11,7 +11,7 @@
 //var electron = require("electron");
 var fs = require("fs");
 var abi = require("node-abi");
-var spawn = require("child_process").spawn;
+var exec = require("child_process").exec;
 
 //Get some information about your environment
 var arch = process.arch;
@@ -26,7 +26,8 @@ process.chdir(`${__dirname}/../node_modules/node-sass`);
 if(fs.existsSync(vendor+"/binding.node")) process.exit();
 
 //Rebuild node-sass with node-gyp
-var gyp = spawn("node-gyp",["rebuild",`--target=${electronVersion}`,`--arch=${arch}`,"--dist-url=https://atom.io/download/electron"],{cwd: process.cwd()});
+console.log(process.cwd());
+var gyp = exec(`node-gyp rebuild --target=${electronVersion} --arch=${arch} --dist-url=https://atom.io/download/electron`,{cwd: process.cwd()});
 //Attach node-gyp to console so you don't have a heart attack when your computer's
 //fans sound like a jet engine
 gyp.stdout.on("data",function(data) {
